ad9c8381d3d2eb8cea19fef023ae059c64adbc1f
maaaz15 <kaifzama@gmail.com>
2024-01-16 16:21:01 +0530

    Add PostForm component and RTE component

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import Logout from "./Header/Logout";
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input"
+import Login from "./Login"
+import RTE from "./RTE"
+import Select from "./Select"
+import Signup from "./Signup"
+import PostCard from "./PostCard"
+import PostForm from "./PostForm/PostForm";
 
 export {
     Header,
@@ -10,4 +19,13 @@ export {
     Container,
     Logo,
     Logout,
+    AuthLayout,
+    Button,
+    Input,
+    Login,
+    RTE,
+    Select,
+    Signup,
+    PostCard,
+    PostForm,
 }
